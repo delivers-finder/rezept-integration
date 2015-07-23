92f4dae6511cc0521b9827d36e400355ee225445
Ludwig Kaube <ludwigkaube@simplora.de>
2015-07-23 15:37:14 +0200

    refined

--- spaghetti.js
+++ spaghetti.js
@@ -6,41 +6,37 @@ $( document ).ready(function() {
 
   $('#my_popup').popup(
     {
-      type: 'tooltip',
+      type: 'overlay',
       transition: 'all 0.3s',
       closetransitionend : function() {
         $('.shop-login-form').hide()
         $('.receipe').show()
-        $('.account-register-inputs').hide()
-        $('.account-login-inputs').hide()
       }
     }
   );
 
   $('#tooltip').popup(
     {
-      type: 'tooltip',
-      transition: 'all 0.3s',
+      type: 'overlay',
       openelement :'.tooltip_open',
       closeelement :'.tooltip_close',
       focuselement : '#tooltip',
       autozindex : true,
-      color: '#fff',
-      vertical : 'bottom',
-      horizontal: 'leftedge'
+      // vertical : 'bottom',
+      // horizontal: 'leftedge'
     }
   );
 
   $('#tooltip2').popup(
     {
-      type: 'tooltip',
+      type: 'overlay',
       transition: 'all 0.3s',
       openelement :'.tooltip2_open',
       closeelement :'.tooltip2_close',
       focuselement : '#tooltip2',
       autozindex : true,
-      color: '#fff',
-      horizontal: 'left'
+      // color: '#fff',
+      // horizontal: 'left'
     }
   );
 
